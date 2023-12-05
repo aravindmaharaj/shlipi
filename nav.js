53c133ee229d7dce02cdc529277d932c69cfb92e
aravindmaharaj <128970121+aravindmaharaj@users.noreply.github.com>
2023-12-05 20:26:10 +0530

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,5 +1,5 @@
 //nav bar skcky
-const body = document.querySelector("body");
+const body = document.querySelector(".body");
 const navbar = document.querySelector(".navbar");
 const menuBtn = document.querySelector(".menu-btn");
 const cancelBtn = document.querySelector(".cancel-btn");
