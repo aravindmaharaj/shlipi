028b8b77c65aab41fe355ea189921a3734543a4a
aravindmaharaj <128970121+aravindmaharaj@users.noreply.github.com>
2023-12-05 21:26:12 +0530

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,16 +1,16 @@
 let bodyy = document.querySelector(".bodyy");
 const navbarr = document.querySelector(".navB");
-const menuBtn = document.querySelector(".menu-btn");
+const menuBtnn = document.querySelector(".menuB");
 const cancelBtn = document.querySelector(".cancel-btn");
 menuBtn.onclick = () => {
     navbarr.classList.add("show");
-    menuBtn.classList.add("hide");
+    menuBtnn.classList.add("hide");
     bodyy.classList.add("disabled");
 }
 cancelBtn.onclick = () => {
     bodyy.classList.remove("disabled");
     navbarr.classList.remove("show");
-    menuBtn.classList.remove("hide");
+    menuBtnn.classList.remove("hide");
 }
 window.onscroll = () => {
     this.scrollY > 20 ? navbarr.classList.add("sticky") : navbarr.classList.remove("sticky");
