527042f759ad39448c12a768fd9f1ac19cd89db2
aravindmaharaj <aravindneruvatla22@gmail.com>
2023-12-05 21:43:26 +0530

    sj dub delet

--- script.js
+++ script.js
@@ -22,29 +22,6 @@ setInterval(showNextImage, 4000); // Change image every 5 seconds
 
 
 
-// Your existing JavaScript code for the sticky navigation bar
-
-
-//nav bar skcky
-const body = document.querySelector("body");
-const navbar = document.querySelector(".navbar");
-const menuBtn = document.querySelector(".menu-btn");
-const cancelBtn = document.querySelector(".cancel-btn");
-menuBtn.onclick = () => {
-    navbar.classList.add("show");
-    menuBtn.classList.add("hide");
-    body.classList.add("disabled");
-}
-cancelBtn.onclick = () => {
-    body.classList.remove("disabled");
-    navbar.classList.remove("show");
-    menuBtn.classList.remove("hide");
-}
-window.onscroll = () => {
-    this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
-}
-
-
 /* Lightbox */
 var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
 
